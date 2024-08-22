0017bea48e8aafae0f872ee36560a8fe16dd2d3c
Shraddha Suryawanshi <109404381+Shraddha4git@users.noreply.github.com>
2024-08-22 15:50:50 +0530

    Update variables.tf

--- Modules-Ec2,S3,DynamoDb-advance/modules/ec2_instance/variables.tf
+++ Modules-Ec2,S3,DynamoDb-advance/modules/ec2_instance/variables.tf
@@ -1,1 +1,30 @@
+variable "ec2_ami_id" {
+  description = "AMI Id for EC2 instance"
+  type = string
+}
+
+variable "ec2_instance_type" {
+  description = "Instance type for EC2 instance"
+  type = string
+}
+
+variable "ec2_instance_name" {
+  description = "Name of the EC2 instance"
+  type = string
+}
+
+variable "ec2_private_key" {
+  description = "EC2 Private Key Path"
+  type = string
+}
+
+variable "ec2_security_group_name" {
+  description = "Name of the Security group for EC2"
+  type = string
+}
+
+variable "vpc_id" {
+  description = "VPC Id"
+  type = string
+}
 
