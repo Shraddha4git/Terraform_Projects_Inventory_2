34728c5f88e02b7a1c5c487ac72dae20d3778bca
Shraddha Suryawanshi <109404381+Shraddha4git@users.noreply.github.com>
2024-08-22 15:44:33 +0530

    Update variables.tf

--- Modules-Ec2,S3,DynamoDb-advance/variables.tf
+++ Modules-Ec2,S3,DynamoDb-advance/variables.tf
@@ -1,1 +1,54 @@
+#Main Files Variables
+
+variable "aws_region" {
+  description = "aws region"
+  type = string
+}
+
+# EC2 Instance Module Variables
+
+variable "ec2_ami_id" {
+  description = "AMI Id for EC2 instance"
+  type = string
+}
+
+variable "ec2_instance_type" {
+  description = "Instance type for EC2 instance"
+  type = string
+}
+
+variable "ec2_instance_name" {
+  description = "Name of the EC2 instance"
+  type = string
+}
+
+variable "vpc_id" {
+  description = "VPC Id"
+  type = string
+}
+
+variable "ec2_security_group_name" {
+  description = "Name of the Security group for EC2"
+  type = string
+}
+
+variable "ec2_private_key" {
+  description = "Path for the EC2 pem file"
+  type = string
+}
+
+# S3 Module Variables
+
+variable "s3_bucket_name" {
+  description = "Name of the s3 bucket"
+  type = string
+}
+
+
+# Dynamodb Module Variables
+
+variable "aws_dynamodb_table_name" {
+    description = "Name of the dynamodb table"
+    type = string
+}
 
