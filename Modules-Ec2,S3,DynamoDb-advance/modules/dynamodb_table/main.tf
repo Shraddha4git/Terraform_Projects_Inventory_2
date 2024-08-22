3e7b28a026a371f7781956fb3176bc85615b5f73
Shraddha Suryawanshi <109404381+Shraddha4git@users.noreply.github.com>
2024-08-22 15:47:50 +0530

    Update main.tf

--- Modules-Ec2,S3,DynamoDb-advance/modules/dynamodb_table/main.tf
+++ Modules-Ec2,S3,DynamoDb-advance/modules/dynamodb_table/main.tf
@@ -1,1 +1,15 @@
+resource "aws_dynamodb_table" "aws_terraform_lock_dynamodb_table" {
+  name = var.aws_dynamodb_table_name
+  hash_key = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    "Name" = "Dev-Table"
+  }
+}
 
