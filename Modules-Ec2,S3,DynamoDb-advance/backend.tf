94cc01e15e75f08655b742549bc861ff24e4cccc
Shraddha Suryawanshi <109404381+Shraddha4git@users.noreply.github.com>
2024-08-22 15:53:29 +0530

    Update backend.tf

--- Modules-Ec2,S3,DynamoDb-advance/backend.tf
+++ Modules-Ec2,S3,DynamoDb-advance/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "web-terraform-backend-bucket"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "web-terraform-lock-table"
   }
